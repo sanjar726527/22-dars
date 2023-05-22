a8d8d3c1648ec763fe8ff21a1472349e6670316f
sanjar726527 <krutoy99sanjara@gmail.com>
2023-05-22 17:51:34 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Box, Stack } from "@mui/material";
 import { Feed } from "./components/Feed";
 import { Sidebar } from "./components/Sidebar";
 import { Navbar } from "./components/Navbar";
-import { Flip, Slide, Bounce, Zoom } from "react-awesome-reveal";
+import { Bounce } from "react-awesome-reveal";
 
 function App() {
   return (
